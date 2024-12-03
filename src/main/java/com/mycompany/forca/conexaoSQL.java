057b4a0f84a10b73931b9edd02ed8a46d03fdd6f
USF Bragança - Laboratório <SP300lab@PEDAGOGICO.NET>
2024-12-03 10:00:00 -0300

    conexao banco

---
+++ src/main/java/com/mycompany/forca/conexaoSQL.java
@@ -0,0 +1,33 @@
+package com.mycompany.forca;
+
+import java.sql.*;
+import javax.swing.JOptionPane;
+import java.util.Random;
+public class conexaoSQL {
+    
+    Connection c;
+    Statement s;
+    ResultSet rs;
+    Random rand = new Random();
+    
+    String url = "jdbc:mysql://mysql.doctusedtech.com.br:3306/doctusedtech07";
+    String user = "doctusedtech07";
+    String password = "AulaUSF2024";
+    //String driver = "com.mysql.jdbc.Driver";
+    public void openConnectionMySql() throws SQLException{
+        try{
+            c = DriverManager.getConnection(url, user, password);
+            s = c.createStatement();
+            int n = rand.nextInt(272);
+            String SQL = "SELECT T2_PALAVRA from T2_PALAVRA WHERE T2_ID =" +n;
+            ResultSet result = s.executeQuery(SQL);
+                    while(result.next()){
+                        System.out.println(result.getString(1));
+                        
+                    }
+        }
+            catch(SQLException e){
+            System.out.println("Falha ao connectar com banco de dados" + e.toString());
+            }
+    }
+}
